704504c502a3d3a532274ea7db40dfb2193310c3
Robert Soriano <sorianorobertc@gmail.com>
2021-10-17 13:33:19 +0800

    refactor: add nuxt3 deps to external

--- packages/nuxt3/siroc.config.ts
+++ packages/nuxt3/siroc.config.ts
@@ -2,6 +2,6 @@ import { defineSirocConfig } from 'siroc';
 
 export default defineSirocConfig({
   rollup: {
-    externals: ['vue']
+    externals: ['vue', 'ohmyfetch', 'h3', '@nuxt/kit']
   }
 });
